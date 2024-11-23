5553a46216f149f8bad2d210dca5d53cc1a6053f
Lucas Bonan <lucasbonan15@gmail.com>
2024-11-23 20:22:29 -0300

    Resolvido conflito de merge nos arquivos styles.ts, index.tsx, routes.tsx, etc.

--- src/components/RestaurantsList/styles.ts
+++ src/components/RestaurantsList/styles.ts
@@ -11,18 +11,3 @@ export const List = styled.ul`
   row-gap: 50px;
   margin-top: 40px;
 `
-
-import styled from 'styled-components'
-
-export const Container = styled.section`
-  padding: 32px 0;
-`
-
-export const List = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  column-gap: 80px;
-  row-gap: 50px;
-
-  margin-top: 40px;
-`
